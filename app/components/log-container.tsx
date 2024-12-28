09389cac002b0805e05684a7bac066664db17f5b
in-ch <sxin2949@naver.com>
2024-12-28 14:32:08 +0900

    chore: 프로젝트 init

---
+++ app/components/log-container.tsx
@@ -0,0 +1,7 @@
+/**
+ * 로그 컨테이너
+ * @returns {React.ReactNode}
+ */
+export default function LogContainer(): React.ReactNode {
+  return <></>;
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="border">
+      <p>Hello world</p>
+      <p>Hello world</p>
+      <p>Hello world</p>
+    </div>
+  );
+}
